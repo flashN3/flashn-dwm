af9bf42c8092d3e91de7aa8ba61e65e11e7973e1
Nejc Rus <nejcrus7@gmail.com>
2024-02-05 14:07:32 +0100

    added patches: systray, cfacts, vanitygaps

--- config.h
+++ config.h
@@ -3,8 +3,26 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const unsigned int gappih = 20;
+static const unsigned int gappiv = 10;
+static const unsigned int gappoh =
+    10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    30; /* vert outer gap between windows and screen edge */
+static int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning =
+    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+          X */
+static const unsigned int systrayonleft =
+    0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst =
+    1; /* 1: if pinning fails, display systray on the first monitor, False:
+          display systray on the last monitor*/
+static const int showsystray = 1; /* 0 means no systray */
+static const int showbar = 1;     /* 0 means no bar */
+static const int topbar = 1;      /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
@@ -39,11 +57,27 @@ static const int resizehints =
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT                                                           \
+  1 /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
-    {"><>", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
+    {"[@]", spiral},
+    {"[\\]", dwindle},
+    {"H[]", deck},
+    {"TTT", bstack},
+    {"===", bstackhoriz},
+    {"HHH", grid},
+    {"###", nrowgrid},
+    {"---", horizgrid},
+    {":::", gaplessgrid},
+    {"|M|", centeredmaster},
+    {">M>", centeredfloatingmaster},
+    {"><>", NULL}, /* no layout function means floating behavior */
+    {NULL, NULL},
 };
 
 /* key definitions */
@@ -79,7 +113,26 @@ static const Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY | ShiftMask, XK_h, setcfact, {.f = +0.25}},
+    {MODKEY | ShiftMask, XK_l, setcfact, {.f = -0.25}},
+    {MODKEY | ShiftMask, XK_o, setcfact, {.f = 0.00}},
     {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY | Mod4Mask, XK_u, incrgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_u, incrgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_i, incrigaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_i, incrigaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_o, incrogaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_o, incrogaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_6, incrihgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_6, incrihgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_7, incrivgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_7, incrivgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_8, incrohgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_8, incrohgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_9, incrovgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_9, incrovgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_0, togglegaps, {0}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_0, defaultgaps, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
@@ -103,8 +156,8 @@ static const Key keys[] = {
  * ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
     /* click                event mask      button          function argument */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
+    {ClkTagBar, MODKEY, Button1, tag, {0}},
+    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
     {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
     {ClkClientWin, MODKEY, Button1, movemouse, {0}},
